9daeb18546b15768c86e1e74b03de22b7397d7ac
khanh100204 <52161503+khanh100204@users.noreply.github.com>
2024-03-11 20:42:18 -0400

    invalid key

--- src/stockcheck/pkg/keyfile/key.go
+++ src/stockcheck/pkg/keyfile/key.go
@@ -3,7 +3,7 @@ package keyfile
 //ConsumerKey Global consumer key for authentication
 var CONSUMERKEY string = "MZ4CRMX8RTHEAH1Q9EJG52TMRBUT0VLW"
 
-//Account number in thinkorswim at the top of the window or via your TD Ameritrade account used for local authentication
+//Account number
 var ACCTNUM string = "21081475112941412"
 
 //TDKEY - TDAmeritrade access token
